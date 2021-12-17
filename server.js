7d21daf6ff1535572c1aac051b7641b6c2033b40
Verity Tsai <veritymissed@gmail.com>
2021-12-17 15:49:27 +0800

    chore(static): update the static file path to ./client having the client side SPA code

--- server.js
+++ server.js
@@ -24,13 +24,13 @@ app.use(
 )
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+// app.set('views', path.join(__dirname, 'views'))
+// app.set('view engine', 'ejs')
 
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, './public')))
+app.use(express.static(path.join(__dirname, './client')))
 
 // app.use(function(req, res, next) {// update to match the domain you will make the request from
 //   res.header("Access-Control-Allow-Origin", "http://localhost:8080")
